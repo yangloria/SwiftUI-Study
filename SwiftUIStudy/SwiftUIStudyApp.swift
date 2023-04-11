570b19e7bc05618cc6b992d289f40157ba973acc
dale <dale.k@kidsnote.com>
2023-04-11 14:17:28 +0900

    add project

---
+++ SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by dale on 2023/04/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
